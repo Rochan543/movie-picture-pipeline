2a7aabb18b615f395949a144374d8cca136751bb
Rochan543 <premrochan@gmail.com>
2026-08-26 10:34:45 +0530

    Update MovieList.js

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -32,11 +32,7 @@ function MovieList({ onMovieClick }) {
 
       <ul>
         {movies.map((movie) => (
-          <li
-            className="movieItem"
-            key={movie.id}
-            onClick={() => onMovieClick(movie)}
-          >
+          <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
             {movie.title}
           </li>
         ))}
