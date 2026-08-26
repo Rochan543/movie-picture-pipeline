d1249c665b464c12234eed663f75d2cd0212aa3a
Rochan543 <premrochan@gmail.com>
2026-08-26 10:32:46 +0530

    Update MovieList.js

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -4,21 +4,44 @@ import axios from 'axios';
 
 function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
-      setMovies(response.data.movies);
-    });
+    axios
+      .get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`)
+      .then((response) => {
+        const movieData = response?.data?.movies;
+
+        if (Array.isArray(movieData)) {
+          setMovies(movieData);
+        } else {
+          setMovies([]);
+          setError('Movie data was not returned in the expected format.');
+        }
+      })
+      .catch((err) => {
+        console.error('Failed to load movies:', err);
+        setMovies([]);
+        setError('Unable to load movies.');
+      });
   }, []);
 
   return (
-    <ul>
-      {movies.map((movie) => (
-        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
-          {movie.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      {error && <p>{error}</p>}
+
+      <ul>
+        {movies.map((movie) => (
+          <li
+            className="movieItem"
+            key={movie.id}
+            onClick={() => onMovieClick(movie)}
+          >
+            {movie.title}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
