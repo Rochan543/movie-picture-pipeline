87e70baf8b0525836b67d1e6812bb9ae7c62497f
VRgarlands <tv199023@gmail.com>
2026-08-26 11:48:49 +0530

    All files

---
+++ starter/frontend/src/components/MovieList.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+function MovieList({ onMovieClick }) {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`)
+      .then((response) => {
+        const movieData = response?.data?.movies;
+
+        if (Array.isArray(movieData)) {
+          setMovies(movieData);
+        } else {
+          setMovies([]);
+          setError('Movie data was not returned in the expected format.');
+        }
+      })
+      .catch((err) => {
+        console.error('Failed to load movies:', err);
+        setMovies([]);
+        setError('Unable to load movies.');
+      });
+  }, []);
+
+  return (
+    <>
+      {error && <p>{error}</p>}
+
+      <ul>
+        {movies.map((movie) => (
+          <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
+            {movie.title}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+MovieList.propTypes = {
+  onMovieClick: PropTypes.func.isRequired,
+};
+
+export default MovieList;
